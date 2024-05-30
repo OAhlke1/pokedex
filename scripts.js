49276e8f8a77e507560eeed3c78e584890c82fa7
OAhlke1 <oahlke1@gmail.com>
2024-05-30 08:45:32 +0200

    range function added

--- scripts.js
+++ scripts.js
@@ -5,19 +5,24 @@ let overlayInner;
 let overlay = document.querySelector('.overlay');
 let data;
 let allCards = [];
+let pokeInfos = [];
 
 async function loadPokeImages () {
     if(localStorage.pokeData) {
-        allCards = JSON.parse(localStorage.pokeData);
-        for(let i=0; i<allCards.length; i++) {
-            document.querySelector('.allCards').innerHTML += allCards[i];
-        }
+        pokeInfos = JSON.parse(localStorage.pokeData);
+        setCards();
     }else {
         for(let i=1; i<lastIndex+1; i++) {
             data = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
             data = await data.json();
+            pokeInfos.push({
+                type: data.types[0].type.name,
+                name: data.species.name,
+                image: data.sprites.front_default,
+                noice: data.cries.latest
+            });
             let card = /* HTML */ `
-                <div class="imgCont displayFlex" onclick="showInOverlay(${i-1})" poketype="${data.types[0].type.name}">
+                <div class="imgCont displayFlex" onclick="showInOverlay(${i-1})">
                     <div class="name displayFlex" style="background-color: ${getBgc(data.types[0].type.name)};">
                         <p>#${i}: ${data.species.name.charAt(0).toUpperCase() + data.species.name.slice(1)}</p>
                     </div>
@@ -31,13 +36,36 @@ async function loadPokeImages () {
                     </div>
                 </div>
             `;
-            allCards.push(card);
+            //pokeInfo.push(card);
             document.querySelector('.allCards').innerHTML += card;
             loadingBar(i);
-            if(i === lastIndex) {
-                localStorage.setItem('pokeData', JSON.stringify(allCards));
-            }
+        }
+    }
+    for(let i=1; i<lastIndex+1; i++) {
+        if(i === lastIndex) {
+            localStorage.setItem('pokeData', JSON.stringify(pokeInfos));
         };
+    }
+}
+
+function setCards() {
+    for(let i=0; i<pokeInfos.length; i++) {
+        let card = /* HTML */ `
+            <div class="imgCont displayFlex" onclick="showInOverlay(${i})">
+                <div class="name displayFlex" style="background-color: ${getBgc(pokeInfos[i].type)};">
+                    <p>#${i+1}: ${pokeInfos[i].name.charAt(0).toUpperCase() + pokeInfos[i].name.slice(1)}</p>
+                </div>
+                <img src="${pokeInfos[i].image}" alt="">
+                <p>Type: ${pokeInfos[i].type}</p>
+                <div class="playNoice displayFlex" onclick="startPlayer(event, '${pokeInfos[i].noice}')">
+                    <p>Noice: </p>
+                    <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <polygon points="7,5 15,10 7,15" fill="white"/>
+                    </svg>
+                </div>
+            </div>
+        `;
+    document.querySelector('.allCards').innerHTML += card;
     }
 }
 
@@ -60,7 +88,21 @@ function showInOverlay(i) {
     overlay.classList.remove('displayNone');
     overlay.classList.add('displayFlex');
     hideAllCards();
-    overlay.innerHTML = allCards[i];
+    overlay.innerHTML = /* HTML */ `
+    <div class="imgCont displayFlex" onclick="showInOverlay(${i})">
+        <div class="name displayFlex" style="background-color: ${getBgc(pokeInfos[i].type)};">
+            <p>#${i+1}: ${pokeInfos[i].name.charAt(0).toUpperCase() + pokeInfos[i].name.slice(1)}</p>
+        </div>
+        <img src="${pokeInfos[i].image}" alt="">
+        <p>Type: ${pokeInfos[i].type}</p>
+        <div class="playNoice displayFlex" onclick="startPlayer(event, '${pokeInfos[i].noice}')">
+            <p>Noice: </p>
+            <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="7,5 15,10 7,15" fill="white"/>
+            </svg>
+        </div>
+    </div>
+`;
 }
 
 function closeOverlay() {
@@ -136,4 +178,37 @@ function getBgc(type) {
         }
     }
     return color;
+}
+
+function searchCards() {
+    let input = document.querySelector('#searchAttributes');
+    let val = input.value;
+    for(let i=0; i<pokeInfos.length; i++) {
+        if(pokeInfos[i].type.includes(val) || pokeInfos[i].name.includes(val)) {
+            document.querySelectorAll('.imgCont')[i].classList.add('displayFlex');
+            document.querySelectorAll('.imgCont')[i].classList.remove('displayNone');
+        }else {
+            document.querySelectorAll('.imgCont')[i].classList.remove('displayFlex');
+            document.querySelectorAll('.imgCont')[i].classList.add('displayNone');
+        }
+    }
+}
+
+function fromTo() {
+    let cards = document.querySelectorAll('.imgCont');
+    let from = +document.querySelector('#from').value === 0 ? 0 : +document.querySelector('#from').value;
+    let to = +document.querySelector('#to').value === 0 ? lastIndex : +document.querySelector('#to').value;
+    let diff = to - from; 
+    showAllCards();
+    switch(diff) {
+        case -1: {
+            return;
+        }
+    }
+    for(let i=0; i<from-1; i++)  {
+        cards[i].classList.add('displayNone');
+    }
+    for(let j=to; j<lastIndex; j++) {
+        cards[j].classList.add('displayNone');
+    }
 }
